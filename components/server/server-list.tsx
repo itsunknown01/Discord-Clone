faa26a4c885d28b7705440844173f718b419e874
Ayushman Gohain <122121887+itsunknown01@users.noreply.github.com>
2024-06-22 05:06:23 +0530

    New start

--- components/server/server-list.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ServerList = () => {
-  return (
-    <div>ServerList</div>
-  )
-}
-
-export default ServerList
--- lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { type ClassValue,clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-import {BiSolidPhoneCall} from "react-icons/bi"
-import { ReactNode } from "react";
-
-export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
